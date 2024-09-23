6d4a00b530ec396422b24f28832eb1671205f3eb
Yazan Kittaneh <yazanakittaneh@gmail.com>
2024-09-23 10:26:54 -0500

    attempt at using nuxt-docs repo to create blog. Doesn't work. Keep getting page._id not found issue.

---
+++ components/layout/PrevNext.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mt-10 border-t pt-8 lg:flex lg:flex-row">
+    <LayoutPrevNextButton :prev-next="prev" side="left" />
+    <span class="flex-1" />
+    <LayoutPrevNextButton :prev-next="next" side="right" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const { prev, next } = useContent();
+</script>
---
+++ pages/docs.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+      class="px-4 py-6 md:px-8"
+      :class="[config.main.padded && 'container']"
+    >
+      <ContentRenderer
+        :key="page._id"
+        :value="page"
+      />
+    </div>
+  </template>
+<script setup lang="ts">
+import { useConfig } from '~/composables/useConfig';
+
+
+
+const { backgroundIsWhite } = useAppConf()
+backgroundIsWhite()
+
+onUnmounted(() => backgroundIsWhite(false))
+const config = useConfig();
+const { page } = useContent()
+
+useSeoMeta({
+  title: `${page.value?.title ?? '404'} - ${config.value.site.name}`,
+  ogTitle: page.value?.title,
+  description: page.value?.description,
+});
+</script>
